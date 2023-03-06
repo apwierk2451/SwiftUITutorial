0c881fbebaa4b1879a39fc7b7a1d12d5ec4f87e9
bonf <apwierk2451@gmail.com>
2023-03-07 01:44:55 +0900

    feat: Tutorial 7
    - 프로필 설정
    - State, EnviromentObject, Published 키워드 이용하여 데이터 처리
    - toolbar, sheet 이용하여 modal 기능 구현

--- Landmarks/Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
-
+    @State private var showingProfile = false
+    
     var body: some View {
         NavigationView {
             List {
@@ -19,13 +20,25 @@ struct CategoryHome: View {
                     .frame(height: 200)
                     .clipped()
                     .listRowInsets(EdgeInsets())
-
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
                     CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
                 .listRowInsets(EdgeInsets())
             }
+            .listStyle(.inset)
             .navigationTitle("Featured")
+            .toolbar {
+                Button {
+                    showingProfile.toggle()
+                } label: {
+                    Label("User Profile", systemImage: "person.crop.circle")
+                }
+            }
+            .sheet(isPresented: $showingProfile) {
+                ProfileHost()
+                    .environmentObject(modelData)
+            }
         }
     }
 }
